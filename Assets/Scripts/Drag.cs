35135ba93a156ecd0f484a2e6f6ff47a0568baec
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-02-10 23:16:25 +0800

    To submit
    a

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -6,38 +6,67 @@ using UnityEngine.Rendering;
 
 public class Drag : MonoBehaviour
 {
-    [SerializeField] private float detectionRange;
+    [SerializeField] private float time;
     [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform playerPosition;
+    [SerializeField] private Transform playerHand;
 
+    public Hand hand;
+
+    bool isDragging = false;
+    float chargeTime = 0f;
     private void Update()
     {
         RayDrag();
     }
     void RayDrag()
     {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
         if (Input.GetMouseButton(0))
         {
-            RaycastHit hit;
-            Ray ray = new Ray(transform.position, transform.forward);
-            if (Physics.Raycast(ray, out hit, detectionRange))
-            {
-                GameObject localItem;
-                localItem = GameObject.FindWithTag("item");
 
-                if (localItem != null)
-                {
-                    localItem.transform.position = playerPosition.position;
-                    Debug.Log("aaa");
-                }
-                Debug.Log("adada");  
+            //Drag object at hand
+            if (Physics.SphereCast(ray, hand.handRange, out hit, hand.handRange, layerMask))
+            {
+                hit.collider.transform.position = Vector3.Lerp(hit.transform.position, playerHand.transform.position, time);
+                isDragging = true;
+                CheckGravity(hit, isDragging);
+
+                chargeTime++;
+               // print("Charge Forcec: " + Mathf.Clamp(chargeTime, 0f, 1000f));
+            }
+            else
+            {
             }
         }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Physics.SphereCast(ray, hand.handRange, out hit, hand.handRange, layerMask))
+            {
+                isDragging = false;
+                Charge(hit, chargeTime);
+                chargeTime = 0;
+                CheckGravity(hit, isDragging);
+            }
+        }
+        
+    }
+
+    void Charge(RaycastHit hit, float chargeTime)
+    {
+        float clampedCharge = Mathf.Clamp(chargeTime, 0, 1000);
+        hit.rigidbody.AddForce(transform.forward * clampedCharge * 2f, ForceMode.Force);
+    }
+    void CheckGravity(RaycastHit hit, bool isDragging)
+    {
+            //checks the gravity
+            hit.rigidbody.useGravity = !isDragging;
+            Debug.Log("Gravity: " + !isDragging);
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, transform.forward * detectionRange);
-        Gizmos.DrawWireSphere(transform.position, 2f);
     }
 }
--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -5,6 +5,8 @@ using UnityEngine.UIElements.Experimental;
 
 public class DragObject : MonoBehaviour
 {
+    [SerializeField] private float drag = 0.5f;
+    [SerializeField] private float angularDrag = 0.5f;
     public Rigidbody body;
     private void Start()
     {
